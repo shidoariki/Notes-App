d28feaa21fa279baffbca9445a6d3ad364dc5259
shidoariki <rushikeshoni@gmail.com>
2025-12-28 13:55:51 +0530

    Fix CORS for production

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 3001;
 // ========================
 // Middleware Setup
 // ========================
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
